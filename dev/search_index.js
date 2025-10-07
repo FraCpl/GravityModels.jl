b6ba31de18522d5a22d31be71617ce63b6341890
Documenter.jl <documenter@juliadocs.github.io>
2025-10-07 19:23:31 +0000

    build based on 4601696

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#GravityModels","page":"Home","title":"GravityModels","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GravityModels.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
